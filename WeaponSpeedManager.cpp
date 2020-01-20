b0e04c1619051bc733e1bcb1f5982dd983fcc5ed
jarari <hazardhax11@gmail.com>
2020-01-20 12:47:48 +0900

    Removed unnecessary magic effect

--- WeaponSpeedManager.cpp
+++ WeaponSpeedManager.cpp
@@ -52,17 +52,13 @@ void WeaponSpeedManager::EvaluateEvent(Actor* a, int evn) {
 	}
 	if (!ConfigManager::GetConfig()[iConfigType::EnableWeaponSpeed].value)
 		return;
-	ActiveEffect* offset_r = Utils::GetActiveEffectFromActor(a, "BingleWeaponSpeedOffset_R");
-	ActiveEffect* offset_l = Utils::GetActiveEffectFromActor(a, "BingleWeaponSpeedOffset_L");
-	if (!offset_r || !offset_l) {
-		_MESSAGE("Weapon speed effects not found in actor %s", Utils::GetName(a->baseForm));
-		return;
-	}
+	float offset_r = 0;
+	float offset_l = 0;
 	//Calculate offset
 	ActorManager::SetCurrentAV(a, "WeaponSpeedMult", 1);
 	ActorManager::SetCurrentAV(a, "LeftWeaponSpeedMult", 1);
-	offset_r->magnitude = 1 - ActorManager::GetAV(a, "WeaponSpeedMult");
-	offset_l->magnitude = 1 - ActorManager::GetAV(a, "LeftWeaponSpeedMult");
+	offset_r = 1 - ActorManager::GetAV(a, "WeaponSpeedMult");
+	offset_l = 1 - ActorManager::GetAV(a, "LeftWeaponSpeedMult");
 
 	tList<ActiveEffect>* list_ae = a->magicTarget.GetActiveEffects();
 	tList<ActiveEffect>::Iterator it = list_ae->Begin();
@@ -71,10 +67,10 @@ void WeaponSpeedManager::EvaluateEvent(Actor* a, int evn) {
 		//Instead of looking for specific for ids, look for what actorvalue does this ActiveEffect change.
 		if (ae->actorValue == ActorManager::GetActorValuePointerFromMap("WeaponSpeedMult")) {
 			float mag = ae->magnitude;
-			if (ae->item && ae->item->formID < 0x06000000)
+			if (mag > 1)
 				mag -= 1.0f;
-			offset_r->magnitude += mag;
-			offset_l->magnitude += mag;
+			offset_r += mag;
+			offset_l += mag;
 		}
 		++it;
 	}
@@ -85,7 +81,7 @@ void WeaponSpeedManager::EvaluateEvent(Actor* a, int evn) {
 	float speed_l = ConfigManager::GetConfig()[weptype_l * 5 + evn + 1].value;
 
 	if (weptype_r != iWepType::None)
-		ActorManager::SetCurrentAV(a, "WeaponSpeedMult", speed_r + offset_r->magnitude);
+		ActorManager::SetCurrentAV(a, "WeaponSpeedMult", speed_r + offset_r);
 	if (weptype_l != iWepType::None)
-		ActorManager::SetCurrentAV(a, "LeftWeaponSpeedMult", speed_l + offset_l->magnitude);
+		ActorManager::SetCurrentAV(a, "LeftWeaponSpeedMult", speed_l + offset_l);
 }
